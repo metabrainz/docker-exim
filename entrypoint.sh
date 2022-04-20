870135210ceae958b309f7299bd8c6c9047d212f
Laurent Monin <github@norz.org>
2022-04-20 16:30:32 +0200

    Fix up entrypoint.sh
    - using ip addr leads to an error with new base image, and is useless
    - new base image supports EXIM4_SMARTHOST, so use it in this wrapper
    - we still set DOMAIN

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
-set -e
+set -Eeuo pipefail
 
-echo "$DOMAIN" > /etc/mailname
-
-opts=(
-	dc_local_interfaces '0.0.0.0 ; ::0'
-	dc_other_hostnames ''
-	dc_relay_nets "$(ip addr show dev eth0 | awk '$1 == "inet" { print $2 }')"
-)
-
-if [ "$GMAIL_GSUITE_RELAY" ]; then
-	opts+=(
-		dc_eximconfig_configtype 'smarthost'
-		dc_smarthost 'smtp-relay.gmail.com::587'
-	)
-elif [ "$GMAIL_USER" -a "$GMAIL_PASSWORD" ]; then
-	# see https://wiki.debian.org/GmailAndExim4
-	opts+=(
-		dc_eximconfig_configtype 'smarthost'
-		dc_smarthost 'smtp.gmail.com::587'
-	)
-	echo "*.google.com:$GMAIL_USER:$GMAIL_PASSWORD" > /etc/exim4/passwd.client
-else
-	opts+=(
-		dc_eximconfig_configtype 'internet'
-	)
+if [ -n "${DOMAIN:-}" ]; then
+	# metabrainz
+	echo "$DOMAIN" > /etc/mailname
 fi
 
-set-exim4-update-conf "${opts[@]}"
+if [ -n "${GMAIL_GSUITE_RELAY:-}" ]; then
+	# compat / metabrainz
+	export EXIM4_SMARTHOST='smtp-relay.gmail.com::587'
+fi
+
+# original code from https://github.com/tianon/dockerfiles/blob/12d542eb8cb44542469aa8f9b943af423b02374f/exim4/docker-entrypoint.sh
+if [ "$1" = 'exim' ]; then
+	if [ -n "${GMAIL_USER:-}" ] && [ -n "${GMAIL_PASSWORD:-}" ]; then
+		# see https://wiki.debian.org/GmailAndExim4
+		export EXIM4_SMARTHOST='smtp.gmail.com::587' \
+			EXIM4_SMARTHOST_USER="$GMAIL_USER" \
+			EXIM4_SMARTHOST_PASSWORD="$GMAIL_PASSWORD"
+	fi
+	unset GMAIL_USER GMAIL_PASSWORD # scrub env of creds
+
+	if [ -n "${EXIM4_SMARTHOST:-}" ]; then
+		set-exim4-update-conf \
+			dc_eximconfig_configtype 'smarthost' \
+			dc_smarthost "$EXIM4_SMARTHOST"
+		if [ -n "${EXIM4_SMARTHOST_USER:-}" ] && [ -n "${EXIM4_SMARTHOST_PASSWORD:-}" ]; then
+			echo "*:$EXIM4_SMARTHOST_USER:$EXIM4_SMARTHOST_PASSWORD" > /etc/exim4/passwd.client
+		fi
+	fi
+	unset EXIM4_SMARTHOST EXIM4_SMARTHOST_USER EXIM4_SMARTHOST_PASSWORD # scrub env of creds
+
+	if [ "$(id -u)" = '0' ]; then
+		mkdir -p /var/spool/exim4 /var/log/exim4 || :
+		chown -R Debian-exim:Debian-exim /var/spool/exim4 /var/log/exim4 || :
+	fi
+
+	if [ "$$" = 1 ]; then
+		set -- tini -- "$@"
+	fi
+fi
 
 exec "$@"
